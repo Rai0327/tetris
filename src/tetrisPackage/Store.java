462f44c21c78ed2454d3a20a06c37d9590455db8
1911277 <1911277@01-19969.pusd.dom>
2024-04-26 11:53:30 -0700

    Added in high score reading and writing with file.

---
+++ src/tetrisPackage/Store.java
@@ -0,0 +1,92 @@
+package tetrisPackage;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Store {
+	String fileName = "./src/tetrisPackage/scores.txt";
+	
+	public static void main (String[] args) {		
+		Store store = new Store();
+		Score highest = store.getHighestScore();
+		Score lowest = store.getLowestScore();
+		
+		System.out.println("The highest score is " + highest.getName() + " with a score of " + highest.getScore() + "!");
+		System.out.println("The lowest score is " + lowest.getName() + " with a score of " + lowest.getScore() + "!");
+	}
+	
+	public void addScore(double score, String name) {
+		try {
+			FileWriter fileWriter = new FileWriter(fileName);
+		    PrintWriter printWriter = new PrintWriter(fileWriter);
+		    printWriter.print(score + " " + name);
+		    printWriter.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+	    	System.out.println("UNABLE TO WRITE TO FILE");
+		}
+	}
+	
+	private ArrayList<Score> scanScores() {
+		try {
+		    File file = new File(fileName);
+		    Scanner scanner = new Scanner(file);
+		    
+			ArrayList<Score> scores = new ArrayList<Score>();
+		    
+		    while (scanner.hasNextLine()) {
+		    	double score = scanner.nextDouble();
+		    	String name = scanner.next();
+		    	
+		    	scores.add(new Score(score, name));
+		    	
+//		    	System.out.println("Scanned in " + name + " with score of " + score);
+		    }
+		    
+		    scanner.close();
+		    
+		    return scores;
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+	    	System.out.println("MISSING FILE");
+		}
+		return null;
+	}
+	
+	public Score getHighestScore() {
+		ArrayList<Score> scores = scanScores();
+		
+		if (scores.size() <= 0) {return new Score(-1, "");}
+		
+		Score highest = scores.get(0);
+		
+		for (Score score : scores) {
+			if (score.getScore() > highest.getScore()) {
+				highest = score;
+			}
+		}
+		
+		return highest;
+	}
+	
+	public Score getLowestScore() {
+		ArrayList<Score> scores = scanScores();
+		
+		if (scores.size() <= 0) {return new Score(-1, "");}
+		
+		Score lowest = scores.get(0);
+		
+		for (Score score : scores) {
+			if (score.getScore() < lowest.getScore()) {
+				lowest = score;
+			}
+		}
+		
+		return lowest;
+	}
+}
