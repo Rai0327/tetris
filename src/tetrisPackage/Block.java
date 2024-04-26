d85e7e93988486afc8225c9d0e4231989760c2fd
1903689 <1903689@01-19975.pusd.dom>
2024-04-26 11:13:19 -0700

    Implemented rotation of tetris blocks

--- src/tetrisPackage/Block.java
+++ src/tetrisPackage/Block.java
@@ -9,46 +9,89 @@ public class Block {
 	private int[][] tile;
 	
 	public Block(int piece) {
-		if (piece == 0) { //Line block
-			int[][] temp = { {1, 1, 1, 1}, 
+//		if (piece == 0) { //Line block
+//			int[][] temp = { {1, 1, 1, 1}, 
+//							 {0, 0, 0, 0}, 
+//							 {0, 0, 0, 0}, 
+//							 {0, 0, 0, 0} };
+//			tile = temp;
+//		} else if (piece == 1) { //Square block
+//			int[][] temp = { {1, 1, 0, 0},
+//							 {1, 1, 0, 0},
+//							 {0, 0, 0, 0},
+//							 {0, 0, 0, 0} };
+//			tile = temp;
+//		} else if (piece == 2) { //T-block
+//			int[][] temp = { {0, 1, 0, 0},
+//							 {1, 1, 1, 0},
+//							 {0, 0, 0, 0},
+//							 {0, 0, 0, 0} };
+//			tile = temp;
+//		} else if (piece == 3) { //J-block
+//			int[][] temp = { {1, 0, 0, 0},
+//							 {1, 1, 1, 0},
+//							 {0, 0, 0, 0},
+//							 {0, 0, 0, 0} };
+//			tile = temp;
+//		} else if (piece == 4) { //L-block
+//			int[][] temp = { {0, 0, 1, 0},
+//							 {1, 1, 1, 0},
+//							 {0, 0, 0, 0},
+//							 {0, 0, 0, 0} };
+//			tile = temp;
+//		} else if (piece == 5) { //S-block
+//			int[][] temp = { {0, 1, 1, 0},
+//							 {1, 1, 0, 0},
+//							 {0, 0, 0, 0},
+//							 {0, 0, 0, 0} };
+//			tile = temp;
+//		} else if (piece == 6) { //Z-block
+//			int[][] temp = { {1, 1, 0, 0},
+//							 {0, 1, 1, 0},
+//							 {0, 0, 0, 0},
+//							 {0, 0, 0, 0} };
+//			tile = temp;
+//		}
+		if (piece == 1) { //Line block
+			int[][] temp = { {0, 0, 0, 0}, 
+							 {1, 1, 1, 1}, 
 							 {0, 0, 0, 0}, 
-							 {0, 0, 0, 0}, 
 							 {0, 0, 0, 0} };
 			tile = temp;
-		} else if (piece == 1) { //Square block
-			int[][] temp = { {1, 1, 0, 0},
-							 {1, 1, 0, 0},
-							 {0, 0, 0, 0},
+		} else if (piece == 2) { //Square block
+			int[][] temp = { {0, 0, 0, 0},
+							 {0, 2, 2, 0},
+							 {0, 2, 2, 0},
 							 {0, 0, 0, 0} };
 			tile = temp;
-		} else if (piece == 2) { //T-block
-			int[][] temp = { {0, 1, 0, 0},
-							 {1, 1, 1, 0},
-							 {0, 0, 0, 0},
+		} else if (piece == 3) { //T-block
+			int[][] temp = { {0, 0, 0, 0},
+							 {0, 3, 0, 0},
+							 {3, 3, 3, 0},
 							 {0, 0, 0, 0} };
 			tile = temp;
-		} else if (piece == 3) { //J-block
-			int[][] temp = { {1, 0, 0, 0},
-							 {1, 1, 1, 0},
-							 {0, 0, 0, 0},
+		} else if (piece == 4) { //J-block
+			int[][] temp = { {0, 0, 0, 0},
+							 {0, 4, 0, 0},
+							 {0, 4, 4, 4},
 							 {0, 0, 0, 0} };
 			tile = temp;
-		} else if (piece == 4) { //L-block
-			int[][] temp = { {0, 0, 1, 0},
-							 {1, 1, 1, 0},
-							 {0, 0, 0, 0},
+		} else if (piece == 5) { //L-block
+			int[][] temp = { {0, 0, 0, 0},
+							 {0, 0, 5, 0},
+							 {5, 5, 5, 0},
 							 {0, 0, 0, 0} };
 			tile = temp;
-		} else if (piece == 5) { //S-block
-			int[][] temp = { {0, 1, 1, 0},
-							 {1, 1, 0, 0},
-							 {0, 0, 0, 0},
+		} else if (piece == 6) { //S-block
+			int[][] temp = { {0, 0, 0, 0},
+							 {0, 6, 6, 0},
+							 {6, 6, 0, 0},
 							 {0, 0, 0, 0} };
 			tile = temp;
-		} else if (piece == 6) { //Z-block
-			int[][] temp = { {1, 1, 0, 0},
-							 {0, 1, 1, 0},
-							 {0, 0, 0, 0},
+		} else if (piece == 7) { //Z-block
+			int[][] temp = { {0, 0, 0, 0},
+							 {0, 7, 7, 0},
+							 {0, 0, 7, 7},
 							 {0, 0, 0, 0} };
 			tile = temp;
 		}
@@ -63,7 +106,7 @@ public class Block {
 		}
 		for (int r = 0; r < tile.length; r++) {
 			for (int c = 0; c < tile[r].length; c++) {
-				tile[r][c] = temp[c][r];
+				tile[r][c] = temp[tile.length - 1 - c][r];
 			}
 		}
 	}
--- src/tetrisPackage/Logic.java
+++ src/tetrisPackage/Logic.java
@@ -1,5 +1,17 @@
 package tetrisPackage;
 
 public class Logic {
-
+	public static void main(String[] args) {
+		Block piece = new Block(7);
+		for (int i = 0; i < 4; i++) {
+			int[][] temp = piece.getTile();
+			for (int r = 0; r < temp.length; r++) {
+				for (int c = 0; c < temp[r].length; c++) {
+					System.out.print(temp[r][c]);
+				}
+				System.out.println();
+			}
+			piece.rotate();
+		}
+	}
 }
